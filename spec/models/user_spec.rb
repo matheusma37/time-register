08725d59dc5c65cc3531ad069d7bf2c51d3f60e8
matheusma37 <mtsma37@gmail.com>
2025-09-10 01:46:19 -0300

    Add user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it "is valid with valid attributes" do
+      user = build(:user)
+
+      expect(user).to be_valid
+    end
+
+    it "is invalid without a name" do
+      user = build(:user, name: nil)
+
+      expect(user).not_to be_valid
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+
+    it "is invalid without an email" do
+      user = build(:user, email: nil)
+
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "is invalid with an incorrect email format" do
+      user = build(:user, email: "invalid_email")
+
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).to include("is invalid")
+    end
+
+    it "does not allow duplicate emails" do
+      create(:user, email: "test@example.com")
+
+      user = build(:user, email: "test@example.com")
+
+      expect(user).not_to be_valid
+      expect(user.errors[:email]).to include("has already been taken")
+    end
+
+    it "does not allow duplicate emails with different letter cases" do
+      create(:user, email: "test@example.com")
+
+      user = build(:user, email: "TEST@example.com")
+
+      expect(user).not_to be_valid
+    end
+  end
+end
