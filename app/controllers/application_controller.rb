36f9aea2771cf4682769a2ecc95a3334698287d0
matheusma37 <mtsma37@gmail.com>
2025-09-10 20:11:48 -0300

    Add default error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from StandardError do |e|
+    render json: { errors: [ e.message ] }, status: :internal_server_error
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { errors: [ e.message ] }, status: :not_found
+  end
 end
