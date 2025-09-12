e149200f0476ccc3b80e6cf63bb3dd195676ef67
matheusma37 <mtsma37@gmail.com>
2025-09-11 23:20:11 -0300

    Install and config SimpleCov

--- Gemfile
+++ Gemfile
@@ -23,4 +23,5 @@ end
 
 group :test do
   gem "shoulda-matchers"
+  gem "simplecov", require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,12 @@
 
 ENV['RAILS_ENV'] = 'test'
 
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  add_filter '/spec/'
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
